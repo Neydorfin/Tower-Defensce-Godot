4be2baec82057da5507aba41a2df4e5118570634
Neydorfin <iongalin.02@gmail.com>
2024-08-16 18:27:30 +0300

    added new controllers and updated tick

--- scripts/runtime/Runner.gd
+++ scripts/runtime/Runner.gd
@@ -6,46 +6,49 @@ var isRunning: bool = false
 
 
 func _process(_delta: float) -> void:
-	if not isRunning:
-		return
-	tickControllers()
+    if not isRunning:
+        return
+    tickControllers(_delta)
 
 
 func startRunning():
-	createAllControllers()
-	onStartControllers()
-	isRunning = true
+    createAllControllers()
+    onStartControllers()
+    isRunning = true
 
 
 func stopRunning():
-	onStopControllers()
-	isRunning = false
+    onStopControllers()
+    isRunning = false
 
 
 func createAllControllers():
-	controllers = []
-	controllers.append(TestController.new())
+    controllers = [
+    GridPointerController.new(GameManager.player.gridHolder),
+    EnemySpawnController.new(GameManager.currentLevel.spawnWavesAsset, GameManager.player.grid),
+    MovementController.new(),
+    ]
 
 
 func onStartControllers():
-	for controller in controllers:
-		if controller:
-			controller.onStart()
-		else:
-			print("Error: %s" % controller.get_name())
+    for controller in controllers:
+        if controller:
+            controller.onStart()
+        else:
+            print("Error: %s" % controller.get_name())
 
 
-func tickControllers():
-	for controller in controllers:
-		if controller:
-			controller.tick()
-		else:
-			print("Error: %s" % controller.get_name())
+func tickControllers(_delta):
+    for controller in controllers:
+        if controller:
+            controller.tick(_delta)
+        else:
+            print("Error: %s" % controller.get_name())
 
 
 func onStopControllers():
-	for controller in controllers:
-		if controller:
-			controller.onStop()
-		else:
-			print("Error: %s" % controller.get_name())
+    for controller in controllers:
+        if controller:
+            controller.onStop()
+        else:
+            print("Error: %s" % controller.get_name())
