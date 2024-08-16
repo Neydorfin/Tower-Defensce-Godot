26fa02c0533a20ff6dbefed5d5a4ad71b3cbbb8e
Neydorfin <iongalin.02@gmail.com>
2024-08-16 18:22:27 +0300

    updated Enemy

--- scripts/enemy/EnemyData.gd
+++ scripts/enemy/EnemyData.gd
@@ -1,1 +1,13 @@
 class_name EnemyData
+
+var _enemy_asset: EnemyAsset
+var enemyView: EnemyView
+
+
+func _init(enemy_asset: EnemyAsset) -> void:
+	_enemy_asset = enemy_asset
+
+
+func attachView(enemy_view: EnemyView) -> void:
+	enemyView = enemy_view
+	enemyView.attachData(self)
--- scripts/enemy/EnemyView.gd
+++ scripts/enemy/EnemyView.gd
@@ -1,2 +1,13 @@
-extends Node
 class_name EnemyView
+extends Node3D
+
+var enemyData: EnemyData
+var movementAgent: InterfaceMovementAgent
+
+
+func attachData(enemy_data: EnemyData) -> void:
+	enemyData = enemy_data
+
+
+func createMovementAgent(grid: Grid) -> void:
+	movementAgent = GridMovementAgent.new(5, self, grid)
