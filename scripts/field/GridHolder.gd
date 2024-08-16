6843bd3ef92cd74d8515ead90dbd6a08aa15a379
Neydorfin <iongalin.02@gmail.com>
2024-08-16 18:24:11 +0300

    updated GridHolder and added for this controller

--- scripts/field/GridHolder.gd
+++ scripts/field/GridHolder.gd
@@ -6,47 +6,43 @@ class_name GridHolder
 @export var CellSize: float
 @export var TargetCoordinate: Vector2i
 @export var StartCoordinate: Vector2i
-
-const RAY_LENGTH : float = 1000.0
-
+const RAY_LENGTH: float = 1000.0
 var grid: Grid
 var Camera: Camera3D
 var Offset: Vector3
 
 
-func _ready():
-	Camera = get_viewport().get_camera_3d()
-	var width : float = GridWidth * CellSize
-	var height : float = GridHeight * CellSize
-	
-	# Plane has double size of 1
-	self.scale = Vector3(width * 0.5, 1, height * 0.5)
-	
-	Offset = self.position - (Vector3(width, 0, height) * 0.5)
-	grid = Grid.new(GridWidth, GridHeight, Offset, CellSize, TargetCoordinate)
-	
-	
-	
-func _physics_process(_delta) -> void:
-	if grid == null or Camera == null:
-		return
-	
-	var space_state: PhysicsDirectSpaceState3D = get_viewport().world_3d.direct_space_state
-	var mousePostion: Vector2 = get_viewport().get_mouse_position()
-	var origin : Vector3 = Camera.project_ray_origin(mousePostion)
-	var end : Vector3 = origin + Camera.project_ray_normal(mousePostion) * RAY_LENGTH
-	var query: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(origin, end)
-	query.collide_with_areas = true
-	var result : Dictionary = space_state.intersect_ray(query)
-	
-	if not result.is_empty():
-		var hit_position = result.position
-		var difference = hit_position - Offset
-		
-		var x : int = int(difference.x / CellSize)
-		var y : int = int(difference.z / CellSize)
-		
-		if Input.is_action_just_pressed("left_click"):
-			var cell : Cell = grid.get_cell(x, y)
-			cell.is_Occupied = !cell.is_Occupied 
-			grid.UpdatePathFinding()
+func createGrid():
+    Camera = get_viewport().get_camera_3d()
+    var width: float  = GridWidth * CellSize
+    var height: float = GridHeight * CellSize
+
+    # Plane has double size of 1
+    self.scale = Vector3(width * 0.5, 1, height * 0.5)
+
+    Offset = self.position - (Vector3(width, 0, height) * 0.5)
+    grid = Grid.new(GridWidth, GridHeight, Offset, CellSize, StartCoordinate, TargetCoordinate)
+
+
+func rayCastInGrid() -> void:
+    if grid == null or Camera == null:
+        return
+
+    var space_state: PhysicsDirectSpaceState3D = get_viewport().world_3d.direct_space_state
+    var mousePostion: Vector2                  = get_viewport().get_mouse_position()
+    var origin: Vector3                        = Camera.project_ray_origin(mousePostion)
+    var end: Vector3                           = origin + Camera.project_ray_normal(mousePostion) * RAY_LENGTH
+    var query: PhysicsRayQueryParameters3D     = PhysicsRayQueryParameters3D.create(origin, end)
+    query.collide_with_areas = true
+    var result: Dictionary                     = space_state.intersect_ray(query)
+
+    if not result.is_empty():
+        var hit_position = result.position
+        var difference   = hit_position - Offset
+
+        var x: int = int(difference.x / CellSize)
+        var y: int = int(difference.z / CellSize)
+
+        grid.selectCoordinate(Vector2i(x, y))
+    else:
+        grid.unselectCell()
---
+++ scripts/field/GridPointerController.gd
@@ -0,0 +1,20 @@
+class_name GridPointerController
+extends InterfaceController
+
+var gridHolder: GridHolder
+
+
+func _init(grid_holder: GridHolder) -> void:
+    gridHolder = grid_holder
+
+
+func onStart() -> void:
+    pass
+
+
+func onStop() -> void:
+    pass
+
+
+func tick(_delta) -> void:
+    gridHolder.rayCastInGrid()
