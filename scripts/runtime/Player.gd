18ec681775e86f18f64d60e2d19310d6438b4c29
Neydorfin <iongalin.02@gmail.com>
2024-08-16 18:26:31 +0300

    created player

--- scripts/runtime/Player.gd
+++ scripts/runtime/Player.gd
@@ -1,2 +1,16 @@
-extends Node
 class_name Player
+extends Node
+
+var enemyDatas: Array[EnemyData]
+var gridHolder: GridHolder
+var grid: Grid
+
+
+func _init() -> void:
+	gridHolder = GameManager.find_child_of_type(GameManager.root, GridHolder)
+	gridHolder.createGrid()
+	grid = gridHolder.grid
+
+
+func enemySpawned(enemy_data: EnemyData) -> void:
+	enemyDatas.append(enemy_data)
