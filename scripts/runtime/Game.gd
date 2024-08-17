f1ea9a8340bdc618169361cfd9c717c240d0ad99
Neydorfin <iongalin.02@gmail.com>
2024-08-17 22:23:55 +0300

    added new controllers

--- scripts/runtime/Game.gd
+++ scripts/runtime/Game.gd
@@ -9,42 +9,42 @@ var root: Node
 
 
 func _ready() -> void:
-    root = get_tree().root
+	root = get_tree().root
 
 
 func find_child_of_type(parent: Node, type: Object) -> Node:
-    for child in parent.get_children():
-        if is_instance_of(child, type):
-            return child
-        var grandchild := find_child_of_type(child, type)
-        if grandchild != null:
-            return grandchild
-    return null
+	for child in parent.get_children():
+		if is_instance_of(child, type):
+			return child
+		var grandchild := find_child_of_type(child, type)
+		if grandchild != null:
+			return grandchild
+	return null
 
 
 func remove_all_children(node: Node) -> void:
-    for child in node.get_children():
-        if not is_instance_of(child, Game):
-            node.remove_child(child)
-            child.queue_free()
+	for child in node.get_children():
+		if not is_instance_of(child, Game):
+			node.remove_child(child)
+			child.queue_free()
 
 
 func setAssetRoot(_assetRoot: AssetRoot) -> void:
-    assetRoot = _assetRoot
+	assetRoot = _assetRoot
 
 
 func startLevel(_levelAsset: LevelAsset) -> void:
-    remove_all_children(get_tree().root)
-    currentLevel = _levelAsset
-    get_tree().root.add_child(_levelAsset.sceneAsset.instantiate())
-    startPlayer()
+	remove_all_children(get_tree().root)
+	currentLevel = _levelAsset
+	get_tree().root.add_child(_levelAsset.sceneAsset.instantiate())
+	startPlayer()
 
 
 func startPlayer():
-    player = Player.new()
-    runner = find_child_of_type(root, Runner)
-    runner.startRunning()
+	player = Player.new()
+	runner = find_child_of_type(root, Runner)
+	runner.startRunning()
 
 
 func stopPlayer():
-    runner.stopRunning()
+	runner.stopRunning()
