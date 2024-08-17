7d8269e97536448336b589c16379010e0bddccfd
Neydorfin <iongalin.02@gmail.com>
2024-08-17 22:24:59 +0300

    updated player constructor(added TurretMarket)

--- scripts/runtime/Player.gd
+++ scripts/runtime/Player.gd
@@ -4,12 +4,14 @@ extends Node
 var enemyDatas: Array[EnemyData]
 var gridHolder: GridHolder
 var grid: Grid
+var turretMarket: TurretMarket
 
 
 func _init() -> void:
 	gridHolder = GameManager.find_child_of_type(GameManager.root, GridHolder)
 	gridHolder.createGrid()
 	grid = gridHolder.grid
+	turretMarket = TurretMarket.new(GameManager.currentLevel.turretMarketAsset)
 
 
 func enemySpawned(enemy_data: EnemyData) -> void:
