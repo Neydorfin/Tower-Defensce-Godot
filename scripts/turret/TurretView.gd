82328104430af5cd4706c02d21ae49e3dc353f3e
Neydorfin <iongalin.02@gmail.com>
2024-08-27 19:10:47 +0300

    added animation for bow

--- scripts/turret/TurretView.gd
+++ scripts/turret/TurretView.gd
@@ -3,6 +3,8 @@ extends Node3D
 
 @export var _tower: Node3D
 @export var projectileOrigin: Node3D
+@export var _animator: AnimationTree
+
 var turretData: TurretData
 
 
@@ -13,4 +15,10 @@ func attachData(turret_data: TurretData) -> void:
 
 func towerLookAt(point: Vector3) -> void:
 	point.y = _tower.position.y
-	_tower.look_at(point)
+	_tower.look_at(point, Vector3(0, 1, 0), true)
+	_tower.rotation.x = 0
+
+
+func animateShot() -> void:
+	_animator["parameters/conditions/shot"] = true
+	_animator["parameters/conditions/shot"] = false
